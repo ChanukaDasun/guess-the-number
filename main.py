c0666bb2973d9e9087606d71140e34a090954acd
ChanukaDasun <cdasun101@gmail.com>
2024-10-11 16:59:50 +0530

    launch-v1.1.0

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import random
 import math
-from modules import check_input_type, return_message
+from modules import check_input_type, return_message, check_int, check_upper_lower_bound
 
 def main():
+    upper_bound = input("Enter upper bound of the range: ")
+    upper_bound = check_int(upper_bound)
+    lower_bound = input("Enter lower bound of the range: ")
+    lower_bound = check_int(lower_bound)
 
-    upper_bound = 100
-    lower_bound = 1
+    check_upper_lower_bound(upper_bound, lower_bound)
 
     random_number = random.randint(lower_bound, upper_bound)
 
     minimum_no_of_guesses = math.ceil(math.log2(upper_bound - lower_bound + 1))
+
+    print(f"You've only {minimum_no_of_guesses} chances to guess the integer!")
 
     no_of_guessings = 1
 
@@ -23,8 +28,11 @@ def main():
 
         if input_number == random_number:
             break
+        elif minimum_no_of_guesses == no_of_guessings:
+            print("Insufficient chances!")
 
         no_of_guessings += 1
+
 
 if __name__ == "__main__":
     main()
--- modules.py
+++ modules.py
@@ -11,6 +11,13 @@ def check_input_type(input_value, upper_limit, lower_limit):
 
     return input_value
 
+def check_int(input_value):
+    try:
+        input_value = int(input_value)
+        return input_value
+    except Exception as e:
+        print(f"Error : {e}")
+
 def return_message(user_value, random_value, upper_limit, lower_limit, no_of_guessings):
 
     message = ""
@@ -23,3 +30,12 @@ def return_message(user_value, random_value, upper_limit, lower_limit, no_of_gue
         message += f"Congratulations you did it in {no_of_guessings} try!"
 
     return message
+
+def check_upper_lower_bound(upper_limit, lower_limit):
+    if upper_limit < lower_limit:
+        print("lower limit is higher than upper limit. Invalid inputs!")
+        exit(0)
+    elif upper_limit == lower_limit:
+        print("lower limit and upper limit is equal!")
+        exit(0)
+
